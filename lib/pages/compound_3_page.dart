59823c03e13b5ae70ad4229383dd786277299de9
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-08-04 15:12:03 -0300

    pages created

---
+++ lib/pages/compound_3_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Compound3Page extends StatelessWidget {
+  const Compound3Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/compound_4_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Compound4Page extends StatelessWidget {
+  const Compound4Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

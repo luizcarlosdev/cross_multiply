6619dc4ff3fec372338b54c6951a86183323c248
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-08-05 12:35:07 -0300

    file structure revision

---
+++ lib/pages/compound_four_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CompoundFourPage extends StatelessWidget {
+  const CompoundFourPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}

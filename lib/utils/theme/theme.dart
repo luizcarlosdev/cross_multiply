81ff442f8d7087912e9a0388b96d827a243cc155
Luiz Carlos Dev <luizcarlosdev@gmail.com>
2021-08-05 23:36:55 -0300

    theme revision for input field widget

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final Color black01 = Color(0xFF000000);
 final Color grey01 = Color(0xFF4F4F4F);
@@ -20,7 +21,7 @@ ThemeData basicTheme() {
       ),
       headline6: base.headline6!.copyWith(
         fontFamily: 'Roboto',
-        fontSize: 15.0,
+        fontSize: 24.0,
         color: Colors.green,
       ),
       bodyText1: base.bodyText1!.copyWith(
@@ -39,29 +40,64 @@ ThemeData basicTheme() {
   final ThemeData base = ThemeData.light();
 
   return base.copyWith(
-      textTheme: _basicTextTheme(base.textTheme),
-      primaryColor: Color(0xFF1E311D),
-      indicatorColor: Color(0xFF807A6B),
-      scaffoldBackgroundColor: green02,
-      accentColor: yellow01,
-      buttonColor: Color(0xFF387A36),
-      backgroundColor: Colors.red,
-      iconTheme: IconThemeData(
-        color: Colors.white,
-        size: 20.0,
+    textTheme: _basicTextTheme(base.textTheme),
+    primaryColor: Color(0xFF1E311D),
+    indicatorColor: Color(0xFF807A6B),
+    scaffoldBackgroundColor: green02,
+    accentColor: yellow01,
+    buttonColor: Color(0xFF387A36),
+    backgroundColor: Colors.red,
+    iconTheme: IconThemeData(
+      color: Colors.white,
+      size: 20.0,
+    ),
+    floatingActionButtonTheme: base.floatingActionButtonTheme.copyWith(
+      backgroundColor: green03,
+      foregroundColor: yellow01,
+      hoverColor: green04,
+    ),
+    appBarTheme: base.appBarTheme.copyWith(
+      brightness: Brightness.dark,
+      textTheme: base.textTheme.copyWith(
+        headline6: base.textTheme.headline6?.copyWith(
+          fontFamily: 'Merienda',
+          fontSize: 24.0,
+          color: green06,
+        ),
       ),
-      floatingActionButtonTheme: base.floatingActionButtonTheme.copyWith(
-        backgroundColor: green03,
-        foregroundColor: yellow01,
-        hoverColor: green04,
+    ),
+    tabBarTheme: base.tabBarTheme.copyWith(
+      labelColor: green01,
+      unselectedLabelColor: green06,
+    ),
+    bottomNavigationBarTheme: base.bottomNavigationBarTheme.copyWith(
+      backgroundColor: green01,
+      unselectedItemColor: green06,
+      selectedItemColor: yellow01,
+    ),
+    inputDecorationTheme: base.inputDecorationTheme.copyWith(
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: green06,
+        ),
       ),
-      tabBarTheme: base.tabBarTheme.copyWith(
-        labelColor: green01,
-        unselectedLabelColor: green06,
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: yellow01,
+        ),
       ),
-      bottomNavigationBarTheme: base.bottomNavigationBarTheme.copyWith(
-        backgroundColor: green02,
-        unselectedItemColor: green06,
-        selectedItemColor: yellow01,
-      ));
+      disabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: green05,
+        ),
+      ),
+      labelStyle: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 16.0,
+        color: green01,
+      ),
+      isDense: true,
+      contentPadding: EdgeInsets.symmetric(vertical: 8.0),
+    ),
+  );
 }
